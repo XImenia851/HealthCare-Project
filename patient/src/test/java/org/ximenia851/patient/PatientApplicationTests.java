e54ae212744355ede90e0b5fbd6e780d25cbb235
Ximenia Hagard <ximenia.hagardlaplateforme.io>
2026-05-15 14:36:28 +0200

    feat : start adding files to prepare the tree

---
+++ patient/src/test/java/org/ximenia851/patient/PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ximenia851.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
